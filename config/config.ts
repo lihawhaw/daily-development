fe4000ca3df12f5d60b6f890d016b0df4cb32552
lihawhaw <lihawhaw@163.com>
2022-01-16 02:17:04 +0800

    feat: add RUM

--- config/config.ts
+++ config/config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   routes,
   theme,
   esbuild: {},
-  title: false,
+  title: '前端技术记录',
   ignoreMomentLocale: true,
   // proxy: proxy[REACT_APP_ENV || 'dev'],
   manifest: {
