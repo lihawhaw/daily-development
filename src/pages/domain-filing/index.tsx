59946c709274a716f61a07df03b45f2b314a2282
lihawhaw <lihawhaw@gmail.com>
2022-06-25 00:37:58 +0800

    feat: publish test

--- src/pages/domain-filing/index.tsx
+++ src/pages/domain-filing/index.tsx
@@ -17,7 +17,7 @@ export default function DomainFilingPage() {
   ]
 
   return (
-    <div className={styles.wrapper} data-update-time='202206230000'>
+    <div className={styles.wrapper} data-update-time='202206250000'>
       <header className={styles.header}>
         <h1>极致源于梦想</h1>
       </header>
